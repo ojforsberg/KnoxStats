f15b0969250570303c0f31e00aa3534093f85afb
Ole J. Forsberg <152335411+ojforsberg@users.noreply.github.com>
2026-04-13 17:54:00 -0500

    Add files via upload

---
+++ R/rice.R
@@ -0,0 +1,11 @@
+#' Yields of four varieties of rice
+#'
+#' A dataset containing rice yields (four varieties) planted in 16 plots:
+#'
+#' @format A data frame with 16 rows and 2 variables:
+#' \describe{
+#'   \item{variety}{variety of the rice grown in the plot}
+#'   \item{yield}{yield (in bushels per acre) of the rice in the plot}
+#' }
+#' 
+#' @source Generic data
